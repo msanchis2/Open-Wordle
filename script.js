0ababcb4cf1e5c0b9cf1981ab61aa4a431943e68
Martí Sanchis Román <55256666+msanchis2@users.noreply.github.com>
2022-02-07 01:57:09 +0100

    Mejoras al juego
    -Cambio del cursor al siguiente input automático al escribir una letra
    -Las letras ya usadas aparecen en rojo cuando las escribes de nuevo
    -La longitud de las palabras random siempre es 5

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let personalizado=false;   //activado modo palabra personalizada
 let intentos = [10,8,6,4]; //numero de intentos según dificultad
 let int;                    //numero de intentos de la dificultad seleccionada
 let paso = 0;               //numero de intentos gastados
+let letrasNoCorrectas = [];
 
 window.onload = function (){
     recogerPalabra();
@@ -35,7 +36,10 @@ window.onload = function (){
                 input = document.createElement("input");
                 input.setAttribute("class","inputL p"+i);
                 input.setAttribute("type","text");
-                input.setAttribute("maxlength",1);
+                input.setAttribute("maxlength", 1);
+                input.setAttribute("id", i+""+j);
+                input.addEventListener("keyup", moverCursor);
+                input.addEventListener("change", letraGastada);
                 if(i>0){
                     input.disabled = true;
                 }
@@ -65,6 +69,7 @@ window.onload = function (){
             }else{
                 container.style.background = "gray";
                 correcto = false;
+                letrasNoCorrectas.push(palabraComp[i].value);
             }
         }
         for(let i=0; i<palabraComp.length; i++){
@@ -77,13 +82,14 @@ window.onload = function (){
         if(paso==int){
             paso=0;
             document.getElementById("validar").style.display = "none";
+            alert("La palabra era "+palabra);
         }
         if(correcto){
             letras = document.getElementsByClassName("inputL");
             for(let i=0; i<letras.length; i++){
                 letras[i].disabled = true;
             }
-            alert("has ganado");
+            alert("Has ganado");
         }else{
             letras = document.getElementsByClassName("p"+paso);
             for(let i=0; i<letras.length; i++){
@@ -93,7 +99,7 @@ window.onload = function (){
     });
 }
 
-function asignarTipoJuego(tipoJuego){ //Muestra / oculta la opción de escribir tu propia plabra
+function asignarTipoJuego(tipoJuego){ // Muestra / oculta la opción de escribir tu propia plabra
     personalizado = tipoJuego;
     let display = tipoJuego ? "block":"none";
     document.getElementById("palabra").style.display = display;
@@ -101,11 +107,9 @@ function asignarTipoJuego(tipoJuego){ //Muestra / oculta la opción de escribir 
 
 function recogerPalabra(){ //Obtiene una palabra aleatoria a partir de la API, o bien recoge la personalizada
     if(!personalizado){
-        fetch('https://palabras-aleatorias-public-api.herokuapp.com/random').then((res) => res.json()).then((data) => {
+        fetch('https://palabras-aleatorias-public-api.herokuapp.com/random-by-length?length=5').then((res) => res.json()).then((data) => {
             palabra = data.body.Word;
-            if(palabra.length>6){ 
-                recogerPalabra();
-            }
+            palabra = removeAccents(palabra);
             document.getElementById("inputP").value = palabra;
         });
     }
@@ -118,6 +122,24 @@ function letraEsta(l){//Comprueba si una letra está en la palabra escrita (Most
         if(palabra[j]==l){
             return true;
         }
+    }
+}
+
+function moverCursor(){ //Una vez escribes una letra, mueve el cursor al siguiente input
+    let idletra = this.getAttribute("id")[1];
+    let idpalabra = this.getAttribute("id")[0];
+    let numInput = parseInt(idletra)+1;
+    let input = document.getElementById(idpalabra+""+numInput);
+    if(input!=null && this.value!=""){
+        input.focus();
+    }
+}
+
+function letraGastada(){ //Pone de color rojo una letra si la escribes habiendo comprobado antes que no está
+    if(letrasNoCorrectas.includes(this.value)){
+        this.style.color = "red";
+    }else{
+        this.style.color = "black";
     }
 }
 
